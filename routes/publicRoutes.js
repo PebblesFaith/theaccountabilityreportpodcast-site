db917d24f383f65312ecf0c046d617e9d4bad678
Sarai Hannah Ajai <ajai.sarai.h@gmail.com>
2026-06-03 21:21:56 -0500

    Build public pages for The Accountability Report Podcast website

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -4,14 +4,44 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (request, response) => {
-  response.render("index");
-  
+router.get("/", (req, res) => {
+  res.render("index");
 });
 
-router.get("/about", (request, response) => {
-  response.render("about");
-  
+router.get("/about", (req, res) => {
+  res.render("about");
+});
+
+router.get("/episodes", (req, res) => {
+  res.render("episodes");
+});
+
+router.get("/reports", (req, res) => {
+  res.render("reports");
+});
+
+router.get("/submit-tip", (req, res) => {
+  res.render("submitatip");
+});
+
+router.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+router.get("/disclaimer", (req, res) => {
+  res.render("disclaimer");
+});
+
+router.get("/privacy-policy", (req, res) => {
+  res.render("privacypolicy");
+});
+
+router.get("/editorial-policy", (req, res) => {
+  res.render("editorialpolicy");
+});
+
+router.get("/corrections-policy", (req, res) => {
+  res.render("correctionspolicy");
 });
 
 module.exports = router;
